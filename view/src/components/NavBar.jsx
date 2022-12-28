9bf1995d0e72ff5cc6040d5396351f54b4876e2e
dev-sydney <sydneyotutey279@gmail.com>
2022-12-28 12:48:46 +0000

    Implemented the functionality of hiding/showing the component based on whether the user is logged in or not

--- view/src/components/NavBar.jsx
+++ view/src/components/NavBar.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import authContext from '../contexts/AuthContext';
 import { NavLink } from 'react-router-dom';
+
+import './../styles/navStyle.scss';
+
+const stat = 1;
+/**
+ * This component holds all the links ot various pages in the app
+ * @returns
+ */
 const NavBar = () => {
+  const authContxt = useContext(authContext);
   return (
-    <div>
+    <div className={`${!authContxt.user && 'not-loggedIn'}`}>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/attendances/ongoingAttendances">Ongoing</NavLink>
       <NavLink to="/me">Profile</NavLink>

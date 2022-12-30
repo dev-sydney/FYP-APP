57500e0239cf5628e639608ced4e7baf550cf248
dev-sydney <sydneyotutey279@gmail.com>
2022-12-30 17:54:59 +0000

    Added & removed some classnames for styling, as well as conditonal rendering for second part of the signup form(index & faculty)

--- view/src/pages/SignupPage.jsx
+++ view/src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import authContext from '../contexts/AuthContext';
 import resourceContext from '../contexts/ResourceContext';
-// import './../styles/formStyles.scss';
+import './../styles/formStyles.scss';
 const stat = 1;
 const SignupPage = () => {
   const navigateTo = useNavigate();
@@ -21,70 +21,31 @@ const SignupPage = () => {
     facultyId: 0,
     indexNumber: 0,
   });
+  const [allFieldsComplete, setAllFieldsComplete] = useState(false);
+  const [showSecondStep, setShowStep] = useState(false);
 
   const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormData({ ...formData, [e.target.name]: e.target.value.trim() });
+    if (!Object.values(formData).includes('')) {
+      setAllFieldsComplete(true);
+    }
   };
   const onSubmit = (e) => {
     e.preventDefault();
-
-    authContxt.signupUser(formData, navigateTo);
+    // authContxt.signupUser(formData, navigateTo);TODO://REVERT BACK AFTER FINISHING STYLING
   };
   return (
-    <div className="login-container">
+    <div className="signup-container">
       <p className="form__name">Sign up</p>
       <h1 className="logo">LOGO</h1>
-      SignupPage
-      <form className="auth-form" onSubmit={onSubmit}>
-        {/* -------LAST NAME INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Last name: </label>
-          <input
-            className="form__input"
-            type="text"
-            placeholder="last name"
-            required
-            name="surName"
-            value={formData.surName}
-            onChange={onChange}
-            minLength={'3'}
-            maxLength={'15'}
-          />
-        </div>
 
-        {/* -------OTHER NAMES INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Other names: </label>
-          <input
-            className="form__input"
-            type="text"
-            placeholder="other names"
-            required
-            name="otherNames"
-            value={formData.otherNames}
-            onChange={onChange}
-            minLength={'3'}
-            maxLength={'100'}
-          />
-        </div>
-        {/* -------EMAIL INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Email Address: </label>
-          <input
-            className="form__input"
-            type="email"
-            placeholder="Youremail@example.com"
-            required
-            name="emailAddress"
-            value={formData.emailAddress}
-            onChange={onChange}
-          />
-        </div>
+      <form className="signup__form">
         {/* -------FACULTY SELECT ------- */}
-        <div className="form__group">
-          <label className="form__label">Faculty: </label>
+        <div
+          className={`form__group--signup ${!showSecondStep ? 'hide' : 'show'}`}
+        >
           <select name="facultyId" onChange={onChange} required>
-            <option>---- Please select a faculty ----</option>
+            <option>Faculty </option>
             {/*EDGE-CASE: IF THERES NO FACULTIES LOADED YET RENDER "LOADING..."  */}
             {!resourceContxt.faculties ? (
               <option> loading... </option>
@@ -97,13 +58,75 @@ const SignupPage = () => {
             )}
           </select>
         </div>
-        {/* -------PASSWORD INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Password: </label>
+
+        {/* -------INDEX NUMBER INPUT------- */}
+        <div
+          className={`form__group--signup ${!showSecondStep ? 'hide' : 'show'}`}
+        >
           <input
-            className="form__input"
+            type={'number'}
+            placeholder="Index number eg.12345678"
+            required
+            name="indexNumber"
+            className="form__input--signup"
+            onChange={onChange}
+            minLength={'8'}
+            maxLength={'8'}
+          />
+        </div>
+        {/* -------FIRST & LAST NAME INPUT------- */}
+        <div
+          className={`double__input form__group--signup ${
+            showSecondStep && 'hide'
+          }`}
+        >
+          <input
+            className={`input form__input--signup`}
+            type="text"
+            placeholder="last name"
+            required
+            name="surName"
+            value={formData.surName}
+            onChange={onChange}
+            minLength={'3'}
+            maxLength={'15'}
+          />
+          <input
+            className={`input form__input--signup`}
+            type="text"
+            placeholder="other names"
+            required
+            name="otherNames"
+            value={formData.otherNames}
+            onChange={onChange}
+            minLength={'3'}
+            maxLength={'100'}
+          />
+        </div>
+
+        {/* -------EMAIL INPUT------- */}
+        <div
+          className={`form__group--signup ${showSecondStep ? 'hide' : 'show'}`}
+        >
+          <input
+            className="form__input--signup"
+            type="email"
+            placeholder="Email address"
+            required
+            name="emailAddress"
+            value={formData.emailAddress}
+            onChange={onChange}
+          />
+        </div>
+
+        {/* -------PASSWORD INPUT------- */}
+        <div
+          className={`form__group--signup ${showSecondStep ? 'hide' : 'show'}`}
+        >
+          <input
+            className="form__input--signup"
             type={'password'}
-            placeholder="• • • • • • • •"
+            placeholder="Password"
             required
             name="userPassword"
             value={formData.userPassword}
@@ -111,34 +134,58 @@ const SignupPage = () => {
           />
         </div>
         {/* -------PASSWORD CONFIRM INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Confirm password: </label>
+        <div
+          className={`form__group--signup ${showSecondStep ? 'hide' : 'show'}`}
+        >
           <input
-            className="form__input"
+            className="form__input--signup"
             type={'password'}
-            placeholder="• • • • • • • •"
+            placeholder="Confirm password"
             required
             name="passwordConfirm"
             value={formData.passwordConfirm}
             onChange={onChange}
           />
         </div>
-        {/* -------INDEX NUMBER INPUT------- */}
-        <div className="form__group">
-          <label className="form__label">Index number: </label>
+        {!showSecondStep && (
+          <span className={`login__note`}>
+            Already have an account? <NavLink to={'/login'}>Log in</NavLink>
+          </span>
+        )}
+
+        {/* NOTE: CONDITIONAL RENDERING FOR THE 'NEXT-STEP' & 'SIGN-UP' BUTTONS */}
+        {showSecondStep ? (
+          <span className="back--signup__container">
+            <input
+              type={'button'}
+              value={'Back'}
+              className={'back__btn'}
+              onClick={() => {
+                setShowStep(!showSecondStep);
+              }}
+            />
+
+            <input
+              type={'button'}
+              value="Sign up"
+              className="signup-btn"
+              onClick={onSubmit}
+            />
+          </span>
+        ) : (
           <input
-            className="form__input"
-            type={'number'}
-            placeholder="eg.12345678"
-            required
-            name="indexNumber"
-            value={formData.indexNumber}
-            onChange={onChange}
-            minLength={'8'}
-            maxLength={'8'}
+            disabled={formData.passwordConfirm === '' ? true : false}
+            type={'button'}
+            value="continue"
+            className={`signup-btn continue__btn ${
+              allFieldsComplete ? 'active__btn' : 'inactive__btn'
+            }`}
+            onClick={() => {
+              if (!allFieldsComplete) return;
+              setShowStep(!showSecondStep);
+            }}
           />
-        </div>
-        <input type={'submit'} value="Sign up" />
+        )}
       </form>
     </div>
   );

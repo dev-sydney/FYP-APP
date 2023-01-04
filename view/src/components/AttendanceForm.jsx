0196126b59e5433ea4341a305b8995a083329424
dev-sydney <sydneyotutey279@gmail.com>
2023-01-04 15:30:30 +0000

    Added a drop down arrow  when clicked on, hides the form element,as well as some more class names for styling purposes

--- view/src/components/AttendanceForm.jsx
+++ view/src/components/AttendanceForm.jsx
@@ -2,18 +2,16 @@ import React, { useState, useContext, useEffect } from 'react';
 
 import attendanceContext from '../contexts/AttendanceContext';
 import resourceContext from '../contexts/ResourceContext';
+import { UilAngleDown } from '@iconscout/react-unicons';
 
+import './../styles/attendanceStyle.scss';
 const stat = 1;
 /**
  * The component is the form that the lecturer will use to create start an ongoing attendance
  * @param {*} param0
  * @returns
  */
-const AttendanceForm = ({
-  setIsAttendanceModalActive,
-  isAttendanceModalActive,
-  QRcodeData,
-}) => {
+const AttendanceForm = ({ QRcodeData, setQRcodeData, setDidProfessorScan }) => {
   const attendanceContxt = useContext(attendanceContext);
   const resourceContxt = useContext(resourceContext);
 
@@ -27,7 +25,6 @@ const AttendanceForm = ({
     courseId: 0,
     duration: 5,
   });
-
   /* -------THE SELECT OPTIONS ONCHANGE EVENT HANDLERS------- */
 
   /**
@@ -48,26 +45,43 @@ const AttendanceForm = ({
     e.preventDefault();
     // console.log(formData);
     attendanceContxt.startOngoingAttendance(formData, QRcodeData);
+    setTimeout(() => {
+      setDidProfessorScan(false);
+    }, 200);
   };
 
   return (
-    <div className="test_styling">
-      <h2>Start an attendance for {formData.duration}</h2>{' '}
+    <div className="attendance__form__container">
       <span
         onClick={() => {
-          setIsAttendanceModalActive(!isAttendanceModalActive);
+          setDidProfessorScan(false);
+          setQRcodeData('');
         }}
       >
-        ❌
+        <UilAngleDown color="#ADADAD" size="40" />
       </span>
-      <form onSubmit={onSubmit}>
-        <div className="form_group">
-          <label htmlFor="faculty">Filter by: </label>
+      <form
+        onSubmit={onSubmit}
+        className={'attendance__form'}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div
+          className="form_group"
+          style={{
+            display: 'flex',
+            columnGap: '10px',
+            marginBottom: '1rem',
+          }}
+        >
           <select
             id="faculty"
             name="facultyId"
             required
             onChange={onFacultySelectChange}
+            className={`form__input`}
           >
             <option>faculty</option>
             {/*EDGE-CASE: IF THERES NO FACULTIES LOADED YET RENDER "LOADING..."  */}
@@ -82,9 +96,14 @@ const AttendanceForm = ({
             )}
           </select>
 
-          <label htmlFor="course">Course: </label>
-          <select id="course" name="courseId" required onChange={onChange}>
-            <option>--Please select the course--</option>
+          <select
+            id="course"
+            name="courseId"
+            required
+            onChange={onChange}
+            className={`form__input`}
+          >
+            <option>Course</option>
             {/*EDGE-CASE: IF THERES NO COURSES LOADED YET, RENDER "NO RESULTS FOUND..."  */}
 
             {!resourceContxt.courses || resourceContxt.courses.length === 0 ? (
@@ -99,8 +118,22 @@ const AttendanceForm = ({
           </select>
         </div>
 
-        <div className="form_group">
-          <label>Minutes: </label>
+        <div
+          className="form_group"
+          style={{ margin: '0 1.5em', marginBottom: '1rem' }}
+        >
+          <label style={{ textAlign: 'center' }}>
+            <b
+              style={{
+                fontWeight: '1em',
+                fontSize: '1.5em',
+                textAlign: 'center',
+              }}
+            >
+              {formData.duration}
+            </b>
+            <p>minutes</p>{' '}
+          </label>
           <input
             type={'range'}
             name="duration"
@@ -110,9 +143,12 @@ const AttendanceForm = ({
             required
             onChange={onChange}
             value={formData.duration}
+            className={`form__input`}
           />
         </div>
-        <input type={'submit'} value="Start" />
+        <button className="start__btn" onClick={onSubmit}>
+          start
+        </button>
       </form>
     </div>
   );

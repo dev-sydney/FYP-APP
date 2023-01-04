cfbbe0d26fb71843d4accf11988e0646c7c2b0ed
dev-sydney <sydneyotutey279@gmail.com>
2023-01-04 16:11:43 +0000

    Added an Empty page for testing purposes

---
+++ view/src/pages/Empty.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+const Empty = () => {
+  return (
+    <div>
+      Empty
+      <UilReact size="50" color="#61DAFB" />
+    </div>
+  );
+};
+
+export default Empty;

d9d3de77cc116c535c062b730005e450ea81a778
dev-sydney <sydneyotutey279@gmail.com>
2023-01-04 14:53:47 +0000

    Implemented condtional rendering for some of the links and added icons

--- view/src/components/NavBar.jsx
+++ view/src/components/NavBar.jsx
@@ -1,6 +1,14 @@
-import React, { useContext, useEffect } from 'react';
-import authContext from '../contexts/AuthContext';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+  UilEstate,
+  UilProcess,
+  UilAnalytics,
+  UilUserCircle,
+  UilCreateDashboard,
+} from '@iconscout/react-unicons';
+
+import authContext from '../contexts/AuthContext';
 
 import './../styles/navStyle.scss';
 
@@ -11,28 +19,39 @@ import './../styles/navStyle.scss';
 const NavBar = () => {
   const authContxt = useContext(authContext);
   return (
-    <div className={`${!authContxt.user && 'not-loggedIn'}`}>
-      <NavLink to="/">Home</NavLink>
-      {/* NOTE: CONDITTIONAL RENDERING FOR PRIVILEGE-SPECIFIC LINKS (for professors and heads of dept.) */}
-      {authContxt.user &&
-        (['professor', 'head_of_department'].includes(
-          authContxt.user.privilege
-        ) ? (
-          <span>
-            <NavLink to="/attendances/ongoingAttendances">Ongoing</NavLink>
-            <NavLink to="/attendance-scores">Scores</NavLink>
-          </span>
-        ) : (
-          ''
-        ))}
-
-      <NavLink to="/me">Profile</NavLink>
+    <div className={`nav__bar ${!authContxt.user && 'not-loggedIn'}`}>
+      //TODO: REMOVE AFTER STYLING
+      <NavLink to="/">
+        <UilEstate size="35" color="#1D2021" />
+      </NavLink>
+      {/* NOTE: CONDTIONAL RENDERING FOT THE ONGOING ATTENDANCES LINK */}
+      {['professor', 'head_of_department'].includes(
+        authContxt.user.privilege
+      ) && (
+        <NavLink to="/attendances/ongoingAttendances">
+          <UilProcess size="35" color="#1D2021" />
+        </NavLink>
+      )}
+      {/* NOTE: CONDTIONAL RENDERING FOT THE ONGOING ATTENDANCES LINK */}
+      {['professor', 'head_of_department'].includes(
+        authContxt.user.privilege
+      ) && (
+        <NavLink to="/attendance-scores">
+          <UilAnalytics size="35" color="#1D2021" />
+        </NavLink>
+      )}
+      {/* NOTE: CONDITIONAL RENDERING FOR RESOURCE MANAGER LINK */}
       {authContxt.user &&
         (['admin', 'head_of_department'].includes(authContxt.user.privilege) ? (
-          <NavLink to="/resourceManager">Resources</NavLink>
+          <NavLink to="/resourceManager">
+            <UilCreateDashboard size="35" color="#1D2021" />
+          </NavLink>
         ) : (
           ''
         ))}
+      <NavLink to="/me">
+        <UilUserCircle size="35" color="#1D2021" />
+      </NavLink>
     </div>
   );
 };

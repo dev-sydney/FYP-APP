7cd7eb0f7bc008ad9c575c4751ce7d426c20666c
dev-sydney <sydneyotutey279@gmail.com>
2023-01-05 00:45:07 +0000

    Implemented some condtional rendering to display whether an attendance is ongoing or completed, as well as some new class names for styling

--- view/src/components/OngoingAttendances.jsx
+++ view/src/components/OngoingAttendances.jsx
@@ -1,20 +1,27 @@
 import React, { useContext, useEffect } from 'react';
-import attendanceContext from './../contexts/AttendanceContext';
+import { UilTimesCircle } from '@iconscout/react-unicons';
+import { UilEllipsisH } from '@iconscout/react-unicons';
 import { Link, useNavigate } from 'react-router-dom';
+import attendanceContext from './../contexts/AttendanceContext';
 import './../styles/attendanceStyle.scss';
-
+const stat = 1;
 const OngoingAttendances = () => {
   const attendanceContxt = useContext(attendanceContext);
   const navigateTo = useNavigate();
 
   useEffect(() => {
     attendanceContxt.getAttendancesStarted(navigateTo);
-  }, []);
+  }, [stat]);
 
   const onCancelBtnClick = (ongoingAttendanceId) => () => {
     if (!attendanceContxt.ongoingAttendances) return;
     attendanceContxt.eraseOngoingAttendance(ongoingAttendanceId);
   };
+
+  const userLocale =
+    navigator.languages && navigator.languages.length
+      ? navigator.languages[0]
+      : navigator.language;
   return (
     <div className="ongoing__container">
       {/* ----CONDITIONAL RENDERING FOR THE ONGOING ATTENDANCES---- */}
@@ -28,19 +35,47 @@ const OngoingAttendances = () => {
                 <Link
                   to={`/attendances/ongoingAttendances/${attendance.ongoingAttendanceId}/${attendance.courseId}`}
                 >
-                  <h2>{attendance.lectureRoom}</h2>
-                  <p>Date: {new Date(attendance.createdAt).toDateString()}</p>
-                  <p>
-                    from: {new Date(attendance.createdAt).toLocaleTimeString()}
+                  <div className="flex__1">
+                    <h2>{attendance.lectureRoom}</h2>
+                    <span
+                      onClick={onCancelBtnClick(attendance.ongoingAttendanceId)}
+                      className="more"
+                    >
+                      <UilEllipsisH color="#656464" size="20" />
+                    </span>
+                  </div>
+
+                  <p className="flex__2">
+                    {new Date(attendance.createdAt).toDateString()}
                   </p>
 
-                  <p>to: {new Date(attendance.endsAt).toLocaleTimeString()}</p>
+                  <div className="flex__3">
+                    <p>
+                      {new Date(attendance.createdAt).toLocaleTimeString(
+                        userLocale,
+                        { timeStyle: 'short' }
+                      )}{' '}
+                      -{' '}
+                      {new Date(attendance.endsAt).toLocaleTimeString(
+                        userLocale,
+                        {
+                          timeStyle: 'short',
+                        }
+                      )}
+                    </p>
+                    <span
+                      className={`attendance__status ${
+                        new Date(attendance.endsAt).getTime() <= Date.now()
+                          ? 'completed'
+                          : 'ongoing'
+                      }`}
+                    >
+                      {new Date(attendance.endsAt).getTime() <= Date.now()
+                        ? 'Completed'
+                        : 'Ongoing'}
+                    </span>
+                  </div>
                 </Link>
-                <span
-                  onClick={onCancelBtnClick(attendance.ongoingAttendanceId)}
-                >
-                  ❌
-                </span>
               </div>
             ))
           : 'No attendances yet')}

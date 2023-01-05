92b14018e292886a4ab6253005ed9944a6a7a255
dev-sydney <sydneyotutey279@gmail.com>
2023-01-05 18:30:10 +0000

    Implemented conditonal rendering to display an illustration when there are no ongoing attendances,as well as styling for that illustration

--- view/src/components/OngoingAttendances.jsx
+++ view/src/components/OngoingAttendances.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { UilTimesCircle } from '@iconscout/react-unicons';
 import { UilEllipsisH } from '@iconscout/react-unicons';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, NavLink } from 'react-router-dom';
 import attendanceContext from './../contexts/AttendanceContext';
 import './../styles/attendanceStyle.scss';
 const stat = 1;
@@ -26,59 +26,68 @@ const OngoingAttendances = () => {
     <div className="ongoing__container">
       {/* ----CONDITIONAL RENDERING FOR THE ONGOING ATTENDANCES---- */}
       {attendanceContxt.ongoingAttendances &&
-        (attendanceContxt.ongoingAttendances.length > 0
-          ? attendanceContxt.ongoingAttendances.map((attendance) => (
-              <div
-                className="ongoing__card"
-                key={attendance.ongoingAttendanceId}
-              >
-                <Link
-                  to={`/attendances/ongoingAttendances/${attendance.ongoingAttendanceId}/${attendance.courseId}`}
+        attendanceContxt.ongoingAttendances.length > 0 &&
+        attendanceContxt.ongoingAttendances.map((attendance) => (
+          <div className="ongoing__card" key={attendance.ongoingAttendanceId}>
+            <Link
+              to={`/attendances/ongoingAttendances/${attendance.ongoingAttendanceId}/${attendance.courseId}`}
+            >
+              <div className="flex__1">
+                <h2>{attendance.lectureRoom}</h2>
+                <span
+                  onClick={onCancelBtnClick(attendance.ongoingAttendanceId)}
+                  className="more"
                 >
-                  <div className="flex__1">
-                    <h2>{attendance.lectureRoom}</h2>
-                    <span
-                      onClick={onCancelBtnClick(attendance.ongoingAttendanceId)}
-                      className="more"
-                    >
-                      <UilEllipsisH color="#656464" size="20" />
-                    </span>
-                  </div>
-
-                  <p className="flex__2">
-                    {new Date(attendance.createdAt).toDateString()}
-                  </p>
-
-                  <div className="flex__3">
-                    <p>
-                      {new Date(attendance.createdAt).toLocaleTimeString(
-                        userLocale,
-                        { timeStyle: 'short' }
-                      )}{' '}
-                      -{' '}
-                      {new Date(attendance.endsAt).toLocaleTimeString(
-                        userLocale,
-                        {
-                          timeStyle: 'short',
-                        }
-                      )}
-                    </p>
-                    <span
-                      className={`attendance__status ${
-                        new Date(attendance.endsAt).getTime() <= Date.now()
-                          ? 'completed'
-                          : 'ongoing'
-                      }`}
-                    >
-                      {new Date(attendance.endsAt).getTime() <= Date.now()
-                        ? 'Completed'
-                        : 'Ongoing'}
-                    </span>
-                  </div>
-                </Link>
+                  <UilEllipsisH color="#656464" size="20" />
+                </span>
               </div>
-            ))
-          : 'No attendances yet')}
+
+              <p className="flex__2">
+                {new Date(attendance.createdAt).toDateString()}
+              </p>
+
+              <div className="flex__3">
+                <p>
+                  {new Date(attendance.createdAt).toLocaleTimeString(
+                    userLocale,
+                    { timeStyle: 'short' }
+                  )}{' '}
+                  -{' '}
+                  {new Date(attendance.endsAt).toLocaleTimeString(userLocale, {
+                    timeStyle: 'short',
+                  })}
+                </p>
+                <span
+                  className={`attendance__status ${
+                    new Date(attendance.endsAt).getTime() <= Date.now()
+                      ? 'completed'
+                      : 'ongoing'
+                  }`}
+                >
+                  {new Date(attendance.endsAt).getTime() <= Date.now()
+                    ? 'Completed'
+                    : 'Ongoing'}
+                </span>
+              </div>
+            </Link>
+          </div>
+        ))}
+      {/* NOTE: CONDITONAL RENDERING FOR WHEN THERE ARE NO ONGOING ATTENDANCES */}
+      {!attendanceContxt.ongoingAttendances ||
+      attendanceContxt.ongoingAttendances.length <= 0 ? (
+        <div className="no__attendances__container">
+          <img
+            src="/img/empty-box.png"
+            width={'100%'}
+            className="empty__box__illustration"
+          />
+          <h2>No Attendances</h2>
+          <p>Let's get started, shall we ?</p>
+          <NavLink to="/">Start Attendance</NavLink>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 };

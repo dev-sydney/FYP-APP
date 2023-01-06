9bc0b1a763d6fcc0df462d01453786b79bb469de
dev-sydney <sydneyotutey279@gmail.com>
2023-01-06 20:08:00 +0000

    Added new class names for styling purposes

--- view/src/components/SignedAttendances.jsx
+++ view/src/components/SignedAttendances.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from 'react';
 import attendanceContext from '../contexts/AttendanceContext';
-import './../styles/attendanceStyle.scss';
+// import './../styles/attendanceStyle.scss';
+import './../styles/signedAttendanceStyle.scss';
 
+const stat = 1;
 /**
  * This component is a container that holds all the signed attendances by students
  * @param {*} param0
@@ -12,10 +14,10 @@ const SignedAttendances = ({ ongoingAttendanceId }) => {
 
   useEffect(() => {
     attendanceContxt.getSignedAttendances(ongoingAttendanceId);
-  }, [attendanceContxt.signedAttendances]);
+  }, [stat]);
 
   return (
-    <div className="ongoing__container">
+    <div className="signed__container">
       {attendanceContxt.signedAttendances &&
         (attendanceContxt.signedAttendances.length > 0
           ? attendanceContxt.signedAttendances.map((signedAttendance) => (
@@ -23,14 +25,26 @@ const SignedAttendances = ({ ongoingAttendanceId }) => {
                 className="signed__card"
                 key={signedAttendance.signedAttendanceId}
               >
-                <img src={`/img/users/${signedAttendance.photo}`} />
-                <h2>
-                  {signedAttendance.surName &&
-                    signedAttendance.surName.concat(
-                      ` ${signedAttendance.otherNames}`
-                    )}
-                </h2>
-                <p>{signedAttendance.indexNumber}</p>
+                <img
+                  src={`/img/users/${signedAttendance.photo}`}
+                  style={{
+                    maxHeight: '5em',
+                    minHeight: '5em',
+                    minWidth: '5em',
+                    maxWidth: '5em',
+                    borderRadius: '20px',
+                    marginTop: '.1em',
+                  }}
+                />
+                <div className="name__index">
+                  <h2>
+                    {signedAttendance.surName &&
+                      signedAttendance.surName.concat(
+                        ` ${signedAttendance.otherNames}`
+                      )}
+                  </h2>
+                  <p>{signedAttendance.indexNumber}</p>
+                </div>
               </div>
             ))
           : 'No one has signed attendance yet,please come back later')}

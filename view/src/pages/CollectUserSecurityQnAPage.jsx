e691d36f75ee2d981da0fbf55cf6a4df5831cff9
dev-sydney <sydneyotutey279@gmail.com>
2023-01-13 12:04:01 +0000

    This component holds the component that renders the random series of security questions

---
+++ view/src/pages/CollectUserSecurityQnAPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SecurityQForm from '../components/SecurityQForm';
+import './../styles/CollectQnAStyle.scss';
+
+const CollectUserSecurityQnAPage = () => {
+  return (
+    <div className="collectQnA__container">
+      <SecurityQForm />
+    </div>
+  );
+};
+
+export default CollectUserSecurityQnAPage;

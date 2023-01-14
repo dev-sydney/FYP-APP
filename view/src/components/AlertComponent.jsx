84b3b04e977fe32ea6c192868d8ed00ac1b3c1b3
dev-sydney <sydneyotutey279@gmail.com>
2023-01-14 15:47:47 +0000

    Implemented conditional rendering for the alerts from the authContext

--- view/src/components/AlertComponent.jsx
+++ view/src/components/AlertComponent.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { UilExclamationCircle, UilCheckCircle } from '@iconscout/react-unicons';
 import attendanceContext from '../contexts/AttendanceContext';
+import authContext from '../contexts/AuthContext';
+
 import './../styles/componentsStyles.scss';
 /**
  * This component renders alerts throughout the application
@@ -8,14 +10,17 @@ import './../styles/componentsStyles.scss';
  */
 const AlertComponent = () => {
   const attendanceContxt = useContext(attendanceContext);
+  const authContxt = useContext(authContext);
 
   return (
     <div
       className={`alert__container ${
-        attendanceContxt.attendanceAlert ? 'show_alert' : 'hide_alert'
+        attendanceContxt.attendanceAlert || authContxt.authAlertMessage
+          ? 'show_alert'
+          : 'hide_alert'
       }`}
     >
-      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT ICON OF THE COMPONENT */}
+      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT ICON OF THE COMPONENT (attendanceContext) */}
       {attendanceContxt.attendanceAlert &&
         (attendanceContxt.attendanceAlert.type === 'success' ? (
           <UilCheckCircle size="50" color="#12A2F3" />
@@ -23,7 +28,15 @@ const AlertComponent = () => {
           <UilExclamationCircle size="50" color="#F31212" />
         ))}
 
-      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT DETAILS */}
+      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT ICON OF THE COMPONENT (authContext) */}
+      {authContxt.authAlertMessage &&
+        (authContxt.authAlertMessage.type === 'success' ? (
+          <UilCheckCircle size="50" color="#12A2F3" />
+        ) : (
+          <UilExclamationCircle size="50" color="#F31212" />
+        ))}
+
+      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT DETAILS (attendanceContext)*/}
       {attendanceContxt.attendanceAlert && (
         <div className="alert__details">
           <h2
@@ -38,6 +51,23 @@ const AlertComponent = () => {
             {attendanceContxt.attendanceAlert.heading}
           </h2>
           <p>{attendanceContxt.attendanceAlert.detail}</p>
+        </div>
+      )}
+      {/* NOTE: CONDITIONAL RENDERING FOR THE ALERT DETAILS(authContext) */}
+      {authContxt.authAlertMessage && (
+        <div className="alert__details">
+          <h2
+            style={{
+              color: `${
+                authContxt.authAlertMessage.type === 'success'
+                  ? '#12A2F3'
+                  : '#F31212'
+              }`,
+            }}
+          >
+            {authContxt.authAlertMessage.heading}
+          </h2>
+          <p>{authContxt.authAlertMessage.detail}</p>
         </div>
       )}
     </div>

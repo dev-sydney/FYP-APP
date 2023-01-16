e4d9c0973dc40e359559a7e476303122c1d3afff
dev-sydney <sydneyotutey279@gmail.com>
2023-01-16 14:56:59 +0000

    Created an AppAlert class for creating alert object instances through the various contexts in the application

--- view/src/utils/config.js
+++ view/src/utils/config.js
@@ -7,3 +7,20 @@ export const configHeader = {
 };
 
 export const BASE_URL = 'http://127.0.0.1:8080/api/v1';
+
+/**
+ * This class creates  'AppAlert' instances that will be used across the various contexts in the application
+ */
+export class AppAlert {
+  /**
+   *
+   * @param {String} detail The Alert Message itself
+   * @param {String} type whether its a 'success' or 'error
+   */
+  constructor(detail, type) {
+    //Setting the alert heading on what alert type it is
+    this.heading = type === 'success' ? 'Awesome' : 'Uh Oh';
+    this.detail = detail;
+    this.type = type;
+  }
+}

6c78d9a9e3dd7c1de9fddb9e2935f616748e360d
dev-sydney <sydneyotutey279@gmail.com>
2023-01-21 20:45:47 +0000

    Added the AlertComponent for showing alerts, as well the resetting the users password functionality when the form gets submitted

--- view/src/pages/ResetPasswordPage.jsx
+++ view/src/pages/ResetPasswordPage.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { UilSpinnerAlt } from '@iconscout/react-unicons';
+
+import AlertComponent from '../components/AlertComponent';
 
 import './../styles/formStyles.scss';
+import authContext from '../contexts/AuthContext';
 
 const ResetPasswordPage = () => {
   const [formData, setFormData] = useState({
@@ -9,22 +13,34 @@ const ResetPasswordPage = () => {
     passwordConfirm: '',
   });
   const { token } = useParams();
+  const navigateTo = useNavigate();
+
+  const authContxt = useContext(authContext);
 
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (authContxt.isLoading) return;
+    console.log({ formData, token });
+    authContxt.resetUserPassword(formData, token, navigateTo);
+  };
   return (
     <div className="resetpassword-container">
-      <form className="auth__form">
+      <AlertComponent />
+      <form className="auth__form" onSubmit={onSubmit}>
         <div className="form__group">
           <label htmlFor="" className="form__label">
             Password:{' '}
           </label>
           <input
+            name="password"
             onChange={onChange}
             type="password"
             className="form__input"
             placeholder="• • • • • • • •"
+            value={formData.password}
           />
         </div>
         <div className="form__group">
@@ -32,15 +48,27 @@ const ResetPasswordPage = () => {
             Confirm password:{' '}
           </label>
           <input
+            name="passwordConfirm"
             onChange={onChange}
             type="password"
             className="form__input"
             placeholder="• • • • • • • •"
+            value={formData.passwordConfirm}
           />
         </div>
-        <button className="login-btn ">
+        <button className="login-btn " onClick={onSubmit}>
           {/* NOTE:'condtional rendering logic for displaying either the loading animation or 'done'  */}
-          Done
+          {authContxt.isLoading ? (
+            <div className="spinner_icon">
+              <UilSpinnerAlt
+                color="#FFFFFF"
+                size="22"
+                style={{ marginTop: '.2em' }}
+              />
+            </div>
+          ) : (
+            'done'
+          )}
         </button>
       </form>
     </div>

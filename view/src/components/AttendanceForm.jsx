e620ae39b99dff9d18958c0c2f673219eb0fad1e
dev-sydney <sydneyotutey279@gmail.com>
2023-01-26 23:19:52 +0000

    Did some component clean up & replaced the form for setting the particular course with a simple duration counter

--- view/src/components/AttendanceForm.jsx
+++ view/src/components/AttendanceForm.jsx
@@ -1,11 +1,15 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 
 import attendanceContext from '../contexts/AttendanceContext';
-import resourceContext from '../contexts/ResourceContext';
-import { UilAngleDown, UilSpinnerAlt } from '@iconscout/react-unicons';
+
+import {
+  UilAngleDown,
+  UilSpinnerAlt,
+  UilPlus,
+  UilMinus,
+} from '@iconscout/react-unicons';
 
 import './../styles/attendanceStyle.scss';
-const stat = 1;
 /**
  * The component is the form that the lecturer will use to create start an ongoing attendance
  * @param {*} param0
@@ -13,40 +17,15 @@ const stat = 1;
  */
 const AttendanceForm = ({ QRcodeData, setQRcodeData, setDidProfessorScan }) => {
   const attendanceContxt = useContext(attendanceContext);
-  const resourceContxt = useContext(resourceContext);
 
-  useEffect(() => {
-    //LOADS ALL THE FACULTIIES
-    resourceContxt.loadAllFaculties();
-  }, [stat]);
-
-  const [formData, setFormData] = useState({
-    facultyId: 0,
-    courseId: 0,
-    duration: 5,
-  });
-  /* -------THE SELECT OPTIONS ONCHANGE EVENT HANDLERS------- */
-
-  /**
-   *The Faculties Select-Option Input:
-   *This "onChange" event handler loads all the courses belonging to the selected faculty
-   * @param {Object} e The event Object
-   */
-  const onFacultySelectChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-    resourceContxt.loadAllCourses(e.target.value);
-  };
-
-  const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const [duration, setDuration] = useState(5);
 
   const onSubmit = (e) => {
     e.preventDefault();
     // console.log(formData);
     if (attendanceContxt.isLoading) return;
 
-    attendanceContxt.startOngoingAttendance(formData, QRcodeData);
+    attendanceContxt.startOngoingAttendance(duration, QRcodeData);
 
     setTimeout(() => {
       setDidProfessorScan(false);
@@ -63,92 +42,41 @@ const AttendanceForm = ({ QRcodeData, setQRcodeData, setDidProfessorScan }) => {
       >
         <UilAngleDown color="#ADADAD" size="40" />
       </span>
-      <form
-        onSubmit={onSubmit}
-        className={'attendance__form'}
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
+      <form onSubmit={onSubmit} className={'attendance__form'}>
         <div
-          className="form_group"
           style={{
+            width: '100%',
+            justifyContent: 'center',
             display: 'flex',
-            columnGap: '10px',
-            marginBottom: '1rem',
+            margin: '1em 0',
           }}
         >
-          <select
-            id="faculty"
-            name="facultyId"
-            required
-            onChange={onFacultySelectChange}
-            className={`form__input`}
-          >
-            <option>faculty</option>
-            {/*EDGE-CASE: IF THERES NO FACULTIES LOADED YET RENDER "LOADING..."  */}
-            {!resourceContxt.faculties ? (
-              <option> loading... </option>
-            ) : (
-              resourceContxt.faculties.map((faculty) => (
-                <option value={faculty.facultyId} key={faculty.facultyId}>
-                  {faculty.facultyName}
-                </option>
-              ))
-            )}
-          </select>
+          <div className="duration__controls">
+            <div
+              onClick={() => {
+                setDuration(duration <= 5 ? duration : duration - 5);
+              }}
+              className="control__icon"
+            >
+              <UilMinus size="30" color="#000000" />
+            </div>
 
-          <select
-            id="course"
-            name="courseId"
-            required
-            onChange={onChange}
-            className={`form__input`}
-          >
-            <option>Course</option>
-            {/*EDGE-CASE: IF THERES NO COURSES LOADED YET, RENDER "NO RESULTS FOUND..."  */}
+            <div style={{ margin: '0 0.4em' }}>
+              <b className="duration__value">{duration}</b>
+              <p>minutes</p>
+            </div>
 
-            {!resourceContxt.courses || resourceContxt.courses.length === 0 ? (
-              <option> No results found </option>
-            ) : (
-              resourceContxt.courses.map((course) => (
-                <option value={course.courseId} key={course.courseId}>
-                  {course.courseName}
-                </option>
-              ))
-            )}
-          </select>
-        </div>
-
-        <div
-          className="form_group"
-          style={{ margin: '0 1.5em', marginBottom: '1rem' }}
-        >
-          <label style={{ textAlign: 'center' }}>
-            <b
-              style={{
-                fontWeight: '1em',
-                fontSize: '1.5em',
-                textAlign: 'center',
+            <div
+              className="control__icon"
+              onClick={() => {
+                setDuration(duration >= 20 ? duration : duration + 5);
               }}
             >
-              {formData.duration}
-            </b>
-            <p>minutes</p>{' '}
-          </label>
-          <input
-            type={'range'}
-            name="duration"
-            max="20"
-            min="5"
-            step={5}
-            required
-            onChange={onChange}
-            value={formData.duration}
-            className={`form__input`}
-          />
+              <UilPlus size="30" color="#000000" />
+            </div>
+          </div>
         </div>
+
         <button className="start__btn" onClick={onSubmit}>
           {attendanceContxt.isLoading ? (
             <div className="spinner_icon">

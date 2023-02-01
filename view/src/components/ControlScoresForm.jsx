16309ba1301866177898c03a18d790876886f9a7
dev-sydney <sydneyotutey279@gmail.com>
2023-02-01 22:50:47 +0000

    removed the falculty select input

--- view/src/components/ControlScoresForm.jsx
+++ view/src/components/ControlScoresForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { UilTimes } from '@iconscout/react-unicons';
 
-import resourceContext from '../contexts/ResourceContext';
 import attendanceContext from '../contexts/AttendanceContext';
 
 import './../styles/scoresStyle.scss';
@@ -12,22 +11,12 @@ import './../styles/profileStyle.scss';
  * @returns JSX form
  */
 const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
-  const resourceContxt = useContext(resourceContext);
   const attendanceContxt = useContext(attendanceContext);
 
   const [formData, setFormData] = useState({
     startDate: '',
     endDate: '',
   });
-  const [courseId, setCourseId] = useState(0);
-  /**
-   *The Faculties Select-Option Input:
-   *This "onChange" event handler loads all the courses belonging to the selected faculty
-   * @param {Object} e The event Object
-   */
-  const onFacultySelectChange = (e) => {
-    resourceContxt.loadAllCourses(e.target.value);
-  };
 
   /**
    *The Date Select-Option Input:
@@ -38,21 +27,11 @@ const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  /**
-   * The courses Select-Option Input:
-   * This "onChange" event handler is responsible for changing the courses selct-option input values
-   * @param {*} e The event Object
-   */
-  const onCourseSelectChange = (e) => {
-    setCourseId(e.target.value);
-  };
-
   /* -------THE FORM ONSUBMIT EVEVNT HANDLERS------- */
   const onsubmit = (e) => {
     e.preventDefault();
     //EDGE-CASE: IF THERES NO SELECTED FACULTY AND COURSE
-    if (courseId === 0) return;
-    attendanceContxt.loadAttendanceScores(formData, courseId);
+    attendanceContxt.loadAttendanceScores(formData);
     setIsModalActive(!isModalActive);
   };
   return (
@@ -94,37 +73,7 @@ const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
         />
       </div>
 
-      <label className="form__label">Faculty & course: </label>
-      <div className="form__group faculty__course">
-        <select onChange={onFacultySelectChange} className={`form__input`}>
-          <option>faculty</option>
-          {/*EDGE-CASE: IF THERES NO FACULTIES LOADED YET RENDER "LOADING..."  */}
-          {!resourceContxt.faculties ? (
-            <option> loading... </option>
-          ) : (
-            resourceContxt.faculties.map((faculty) => (
-              <option value={faculty.facultyId} key={faculty.facultyId}>
-                {faculty.facultyName}
-              </option>
-            ))
-          )}
-        </select>
-
-        <select onChange={onCourseSelectChange} className={`form__input`}>
-          <option>course</option>
-          {/*EDGE-CASE: IF THERES NO COURSES LOADED YET, RENDER "NO RESULTS FOUND..."  */}
-          {!resourceContxt.courses || resourceContxt.courses.length === 0 ? (
-            <option> No results found </option>
-          ) : (
-            resourceContxt.courses.map((course) => (
-              <option value={course.courseId} key={course.courseId}>
-                {course.courseName}
-              </option>
-            ))
-          )}
-        </select>
-      </div>
-      <input type={'submit'} className="sub__btn" />
+      <input type={'submit'} className="sub__btn" onClick={onsubmit} />
     </form>
   );
 };

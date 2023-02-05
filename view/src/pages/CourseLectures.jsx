c8ee90bf40ef00e0d3ea45a9280330ba8b52005e
dev-sydney <sydneyotutey279@gmail.com>
2023-02-05 09:28:48 +0000

    Implemented the dynamic rendering of the course name & the number of weeks passed

--- view/src/pages/CourseLectures.jsx
+++ view/src/pages/CourseLectures.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { UilCalender } from '@iconscout/react-unicons';
-import './../styles/attendanceStyle.scss';
 
+import attendanceContext from '../contexts/AttendanceContext';
+
+import './../styles/attendanceStyle.scss';
+const stat = 1;
 /**
  * This Page component renders information about all the signed attendances for a particular course
  * @returns
  */
 const CourseLectures = () => {
+  const attendanceContxt = useContext(attendanceContext);
+  const { courseId } = useParams();
+
+  useEffect(() => {
+    attendanceContxt.getCoursesSignedAttendances(courseId);
+  }, [stat]);
   /**
    * This function gets the number of weeks passed between the current date & the signed attendance date
    * @param {String} startDate The date string
@@ -34,28 +44,29 @@ const CourseLectures = () => {
             borderRadius: '20px',
           }}
         >
-          {[1, 2, 3, 4, 5].map((el) => (
-            <div className="lecture__item">
-              <div className="icon" key={el}>
-                <UilCalender
-                  size="45"
-                  color="#1D6D1D"
-                  style={{
-                    padding: '.5em',
-                    borderRadius: '50%',
-                    background: '#1d6d1d1f',
-                  }}
-                />
-              </div>
-              <div className="title_time">
-                <h3>Operating System</h3>
-                <p>{new Date('2022-11-25T23:21:15.000Z').toDateString()}</p>
-              </div>
-              <p className="time_passed">
-                {getWeeksDiff('2022-11-25T23:21:15.000Z')}
-              </p>
-            </div>
-          ))}
+          {attendanceContxt.attendedLectures &&
+            (attendanceContxt.attendedLectures.length > 0
+              ? attendanceContxt.attendedLectures.map((el) => (
+                  <div className="lecture__item">
+                    <div className="icon" key={el.signedAttendanceId}>
+                      <UilCalender
+                        size="45"
+                        color="#1D6D1D"
+                        style={{
+                          padding: '.5em',
+                          borderRadius: '50%',
+                          background: '#1d6d1d1f',
+                        }}
+                      />
+                    </div>
+                    <div className="title_time">
+                      <h3>{attendanceContxt.currentCourseName}</h3>
+                      <p>{new Date(el.createdAt).toDateString()}</p>
+                    </div>
+                    <p className="time_passed">{getWeeksDiff(el.createdAt)}</p>
+                  </div>
+                ))
+              : 'No Results Found')}
         </div>
       </section>
     </div>

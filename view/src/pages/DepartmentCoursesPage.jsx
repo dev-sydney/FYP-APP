52949ecd6a055842b8709075056c153bb0c59d0f
dev-sydney <sydneyotutey279@gmail.com>
2023-02-06 22:49:07 +0000

    Implemented the dynamic rendering of the department's courses

--- view/src/pages/DepartmentCoursesPage.jsx
+++ view/src/pages/DepartmentCoursesPage.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { UilUserPlus } from '@iconscout/react-unicons';
 
+import resourceContext from '../contexts/ResourceContext';
+
 import './../styles/departmentCoursesStyle.scss';
 
+const stat = 1;
 /**
  * This page component renders all of the courses under a department,
  *  as well as the number of lecturers assigned to each course
  * @returns
  */
 const DepartmentCoursesPage = () => {
+  const resourceContxt = useContext(resourceContext);
+
+  useEffect(() => {
+    resourceContxt.loadDepartmentCourses();
+  }, [stat]);
+
   return (
     <div className="deptCourses__container">
       <section className="headings__section">
@@ -31,97 +40,37 @@ const DepartmentCoursesPage = () => {
       </section>
       <section>
         <div className="dept_courses">
-          <div className="course_item">
-            <Link>
-              <div className="course_title">
-                <h2>Graphic Design</h2>
-              </div>
-              <div className="lecturer_num__photo">
-                <img
-                  src="/img/users/default.jpg"
-                  alt=""
-                  style={{
-                    minHeight: '2em',
-                    maxHeight: '2em',
-                    maxWidth: '2em',
-                    minWidth: '2em',
+          {resourceContxt.departmentCourses &&
+            (resourceContxt.departmentCourses.length > 0
+              ? resourceContxt.departmentCourses.map((deptCourse) => (
+                  <div className="course_item" key={deptCourse.courseId}>
+                    <Link>
+                      <div className="course_title">
+                        <h2>{deptCourse.courseName}</h2>
+                      </div>
+                      <div className="lecturer_num__photo">
+                        <img
+                          src="/img/users/default.jpg"
+                          alt=""
+                          style={{
+                            minHeight: '2em',
+                            maxHeight: '2em',
+                            maxWidth: '2em',
+                            minWidth: '2em',
 
-                    borderRadius: '50%',
-                  }}
-                />
-                <div className="lecturers_num">5+</div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="course_item">
-            <Link>
-              <div className="course_title">
-                <h2>Operating Sytems</h2>
-              </div>
-              <div className="lecturer_num__photo">
-                <img
-                  src="/img/users/default.jpg"
-                  alt=""
-                  style={{
-                    minHeight: '2em',
-                    maxHeight: '2em',
-                    maxWidth: '2em',
-                    minWidth: '2em',
-
-                    borderRadius: '50%',
-                  }}
-                />
-                <div className="lecturers_num">5+</div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="course_item">
-            <Link>
-              <div className="course_title">
-                <h2>Artificial Intelligence</h2>
-              </div>
-              <div className="lecturer_num__photo">
-                <img
-                  src="/img/users/default.jpg"
-                  alt=""
-                  style={{
-                    minHeight: '2em',
-                    maxHeight: '2em',
-                    maxWidth: '2em',
-                    minWidth: '2em',
-
-                    borderRadius: '50%',
-                  }}
-                />
-                <div className="lecturers_num">5+</div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="course_item">
-            <Link>
-              <div className="course_title">
-                <h2>IT Sourcing & Procurment</h2>
-              </div>
-              <div className="lecturer_num__photo">
-                <img
-                  src="/img/users/default.jpg"
-                  alt=""
-                  style={{
-                    minHeight: '2em',
-                    maxHeight: '2em',
-                    maxWidth: '2em',
-                    minWidth: '2em',
-
-                    borderRadius: '50%',
-                  }}
-                />
-                <div className="lecturers_num">5+</div>
-              </div>
-            </Link>
-          </div>
+                            borderRadius: '50%',
+                          }}
+                        />
+                        <div className="lecturers_num">
+                          {deptCourse.AssignedLecturers <= 1
+                            ? `${deptCourse.AssignedLecturers}+`
+                            : `${deptCourse.AssignedLecturers - 1}+`}
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                ))
+              : '')}
         </div>
       </section>
     </div>

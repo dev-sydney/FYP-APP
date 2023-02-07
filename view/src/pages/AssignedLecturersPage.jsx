bd17a8bdf654f7c69ba576dfbe704aa41dc0ade8
dev-sydney <sydneyotutey279@gmail.com>
2023-02-07 18:26:47 +0000

    Implemented the dynamic rendering of the assigned lecturers to a course

--- view/src/pages/AssignedLecturersPage.jsx
+++ view/src/pages/AssignedLecturersPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import { UilUserPlus, UilTimes } from '@iconscout/react-unicons';
+
+import resourceContext from '../contexts/ResourceContext';
 
 import './../styles/departmentCoursesStyle.scss';
 
+const stat = 1;
+/**
+ * This page component renders all the professors assigned to a course,
+ * it also handles the functionality of the assigning courses to other lecturers
+ * @returns
+ */
 const AssignedLecturersPage = () => {
+  const resourceContxt = useContext(resourceContext);
+  const { courseId } = useParams();
+
+  useEffect(() => {
+    resourceContxt.loadAssignedProfessors(courseId);
+  }, [stat]);
+
   return (
     <div className="assigned__container">
       <section className="headings__section">
@@ -19,108 +35,61 @@ const AssignedLecturersPage = () => {
           />
         </div>
         <h1>
-          Manage <br />
+          Assigned <br />
           Professors
         </h1>
       </section>
 
       <section className="lists__section">
         <div className="list__items">
-          <div className="profItem">
-            <div className="prof__info">
-              <img
-                src={`/img/users/user-2-1669769802456.jpeg`}
-                style={{
-                  maxHeight: '4em',
-                  minHeight: '4em',
-                  minWidth: '4em',
-                  maxWidth: '4em',
-                  borderRadius: '20px',
-                  marginTop: '.4em',
-                }}
-                className="prof_photo"
-              />
-              <div className="name__privilege">
-                <h2>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
-                  dolorem sunt temporibus molestias quibusdam, error, repellat
-                  quae quidem est quisquam atque. Perspiciatis excepturi
-                  corrupti omnis nihil velit, blanditiis fugiat placeat!
-                </h2>
-                <p>lecturer</p>
-              </div>
-              <div
-                className="list_controls"
-                style={{ display: 'flex', marginLeft: 'auto' }}
-              >
-                <div
-                  className="deallocate_icon"
-                  //   style={{ marginRight: '.4em' }}
-                >
-                  <UilTimes
-                    size="40"
-                    color="#FF0000"
-                    style={{
-                      padding: '.5em',
-                      borderRadius: '50%',
-                      backgroundColor: '#ca14141a',
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="profItem">
-            <div className="prof__info">
-              <img
-                src={`/img/users/user-2-1669769802456.jpeg`}
-                style={{
-                  maxHeight: '4em',
-                  minHeight: '4em',
-                  minWidth: '4em',
-                  maxWidth: '4em',
-                  borderRadius: '20px',
-                  marginTop: '.4em',
-                }}
-                className="prof_photo"
-              />
-              <div className="name__privilege">
-                <h2>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
-                  dolorem sunt temporibus molestias quibusdam, error, repellat
-                  quae quidem est quisquam atque. Perspiciatis excepturi
-                  corrupti omnis nihil velit, blanditiis fugiat placeat!
-                </h2>
-                <p>lecturer</p>
-              </div>
-            </div>
-          </div>
-          <div className="profItem">
-            <div className="prof__info">
-              <img
-                src={`/img/users/user-2-1669769802456.jpeg`}
-                style={{
-                  maxHeight: '4em',
-                  minHeight: '4em',
-                  minWidth: '4em',
-                  maxWidth: '4em',
-                  borderRadius: '20px',
-                  marginTop: '.4em',
-                }}
-                className="prof_photo"
-              />
-              <div className="name__privilege">
-                <h2>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
-                  dolorem sunt temporibus molestias quibusdam, error, repellat
-                  quae quidem est quisquam atque. Perspiciatis excepturi
-                  corrupti omnis nihil velit, blanditiis fugiat placeat!
-                </h2>
-                <p>lecturer</p>
-              </div>
-            </div>
-          </div>
+          {resourceContxt.assignedProfessors &&
+            (resourceContxt.assignedProfessors.length > 0
+              ? resourceContxt.assignedProfessors.map((assignedProf) => (
+                  <div className="profItem" key={assignedProf.assignmentId}>
+                    <div className="prof__info">
+                      <img
+                        src={`/img/users/${assignedProf.photo}`}
+                        style={{
+                          maxHeight: '4em',
+                          minHeight: '4em',
+                          minWidth: '4em',
+                          maxWidth: '4em',
+                          borderRadius: '20px',
+                          marginTop: '.4em',
+                        }}
+                        className="prof_photo"
+                      />
+                      <div className="name__privilege">
+                        <h2>
+                          {assignedProf.surName.concat(
+                            ` ${assignedProf.otherNames}`
+                          )}
+                        </h2>
+                        <p>{assignedProf.privilege.replaceAll('_', ' ')}</p>
+                      </div>
+                      <div
+                        className="list_controls"
+                        style={{ display: 'flex', marginLeft: 'auto' }}
+                      >
+                        <div
+                          className="deallocate_icon"
+                          //   style={{ marginRight: '.4em' }}
+                        >
+                          <UilTimes
+                            size="40"
+                            color="#FF0000"
+                            style={{
+                              padding: '.5em',
+                              borderRadius: '50%',
+                              backgroundColor: '#ca14141a',
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : 'No Lecturers Assigned Yet.')}
         </div>
       </section>
     </div>

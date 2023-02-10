99802cb6da06b927eac46cbfba04e6f357bb0bf8
dev-sydney <sydneyotutey279@gmail.com>
2023-02-10 13:21:31 +0000

    Implemented the conditional rendering of the select-option input to be rendered if the user has been assigned multiple courses

--- view/src/components/ControlScoresForm.jsx
+++ view/src/components/ControlScoresForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { UilTimes } from '@iconscout/react-unicons';
 
 import attendanceContext from '../contexts/AttendanceContext';
+import resourceContext from '../contexts/ResourceContext';
 
 import './../styles/scoresStyle.scss';
 import './../styles/profileStyle.scss';
@@ -12,10 +13,15 @@ import './../styles/profileStyle.scss';
  */
 const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
   const attendanceContxt = useContext(attendanceContext);
+  const resourceContxt = useContext(resourceContext);
 
   const [formData, setFormData] = useState({
     startDate: '',
     endDate: '',
+    courseId:
+      resourceContxt?.userAssignedCourses.length === 1
+        ? resourceContxt?.userAssignedCourses[0].courseId
+        : 0,
   });
 
   /**
@@ -23,14 +29,16 @@ const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
    *This "onChange" event handler is responsible for changing the date input values
    * @param {*} e The event Object
    */
-  const onDateInputsChange = (e) => {
+  const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   /* -------THE FORM ONSUBMIT EVEVNT HANDLERS------- */
   const onsubmit = (e) => {
     e.preventDefault();
-    //EDGE-CASE: IF THERES NO SELECTED FACULTY AND COURSE
+    //EDGE-CASE: If no course was selected
+    if (formData.courseId === 0 || formData.courseId === '') return;
+
     attendanceContxt.loadAttendanceScores(formData);
     setIsModalActive(!isModalActive);
   };
@@ -48,18 +56,37 @@ const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
         <img src={`/img/businessman-explaining-the-strategy.png`} />
         <h1>Let's Get Started.</h1>
         <p>
-          Choose a start & end date, as well as the faculty to get the specific
-          course you want
+          Choose a start & end date, as well as a course If you've been assigned
+          multiple courses
         </p>
       </div>
+      {resourceContxt.userAssignedCourses &&
+        (resourceContxt.userAssignedCourses.length > 1 ? (
+          <div style={{ margin: '0.7em 0' }}>
+            <label className="form__label">Courses:</label>
+            <select className="form__input" onChange={onChange} name="courseId">
+              <option value="">Pick a course</option>
+              {resourceContxt.userAssignedCourses.map((el) => (
+                <option value={el.courseId} key={el.courseId}>
+                  {el.courseName}
+                </option>
+              ))}
+            </select>
+          </div>
+        ) : (
+          ''
+        ))}
       <label className="form__label">Dates: </label>
-      <div className="form__group start__end__dates">
+      <div
+        className="form__group start__end__dates"
+        style={{ marginBottom: '1em' }}
+      >
         <input
           required
           name="startDate"
           type={'datetime-local'}
           value={formData.startDate}
-          onChange={onDateInputsChange}
+          onChange={onChange}
           className={`form__input start__date`}
         />
         <span className="dash">-</span>
@@ -68,7 +95,7 @@ const ControlScoresForm = ({ isModalActive, setIsModalActive }) => {
           name="endDate"
           type={'datetime-local'}
           value={formData.endDate}
-          onChange={onDateInputsChange}
+          onChange={onChange}
           className={`form__input end__date`}
         />
       </div>

382948078709af39fa8575818e3833ac368c4286
dev-sydney <sydneyotutey279@gmail.com>
2023-02-13 20:23:31 +0000

    Initial commit - Page for rendering the users account information

---
+++ view/src/pages/AccountOverviewPage.jsx
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UilEditAlt } from '@iconscout/react-unicons';
+
+import authContext from '../contexts/AuthContext';
+
+import './../styles/profileStyle.scss';
+
+const AccountOverviewPage = () => {
+  const authContxt = useContext(authContext);
+  const navigateTo = useNavigate();
+
+  return (
+    <div className="overview__container">
+      <div className="row">
+        <h3>Account Overview</h3>
+      </div>
+      <div className="row">
+        {authContxt.user && (
+          <img
+            role={'presentation'}
+            loading="lazy"
+            src={`/img/users/${authContxt.user.photo}`}
+            style={{
+              maxHeight: '8em',
+              minHeight: '8em',
+              minWidth: '8em',
+              maxWidth: '8em',
+              borderRadius: '50%',
+            }}
+          />
+        )}
+      </div>
+
+      <div className="row">
+        <h1>
+          {authContxt.user &&
+            authContxt.user.otherNames.concat(` ${authContxt.user.surName}`)}
+        </h1>
+      </div>
+      <p className="labels" style={{ marginTop: '2em' }}>
+        Your display name
+      </p>
+      <div className="row displayname">
+        <h3>
+          {authContxt.user &&
+            authContxt.user.otherNames.concat(` ${authContxt.user.surName}`)}
+        </h3>
+        <Link to="/account/profile" style={{ marginLeft: 'auto' }}>
+          <UilEditAlt color="#284b63" size="30" />
+        </Link>
+      </div>
+      <p className="labels">Your email address</p>
+      <div className="row displayname">
+        <h3>{authContxt.user ? authContxt.user.emailAddress : ''}</h3>
+        <Link to="/account/profile" style={{ marginLeft: 'auto' }}>
+          <UilEditAlt color="#284b63" size="30" />
+        </Link>
+      </div>
+      <div className="row" style={{ marginTop: '1em' }}>
+        <p>
+          <Link
+            className="password_link overview_btn"
+            to="/account/change-password"
+          >
+            Change Password
+          </Link>
+        </p>
+      </div>
+      <div className="row">
+        <button
+          className="overview_btn logout"
+          onClick={() => {
+            authContxt.signUserOut(navigateTo);
+          }}
+        >
+          Log out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AccountOverviewPage;
--- view/src/pages/LoginPage.jsx
+++ view/src/pages/LoginPage.jsx
@@ -5,7 +5,9 @@ import { UilSpinnerAlt } from '@iconscout/react-unicons';
 import authContext from '../contexts/AuthContext';
 import AlertComponent from '../components/AlertComponent';
 
-import './../styles/formStyles.scss';
+// import './../styles/formStyles.scss';
+import './../styles/profileStyle.scss';
+
 const stat = 1;
 const LoginPage = () => {
   const navigateTo = useNavigate();
@@ -31,36 +33,47 @@ const LoginPage = () => {
   return (
     <div className="login-container">
       <AlertComponent />
-      <p className="form__name">login</p>
-      <h1 className="logo">LOGO</h1>
-      <form onSubmit={onSubmit} className={'auth__form'}>
+
+      <div style={{ textAlign: 'center' }}>
+        <p className="form__name">login</p>
+      </div>
+
+      <div style={{ textAlign: 'center' }}>
+        <h1 className="logo">LOGO</h1>
+      </div>
+
+      <form onSubmit={onSubmit} style={{ padding: '0 1%' }}>
         {/* EMAIL-ADDRESS INPUT */}
-        <div className="form__group">
-          <label className="form__label">Email Address: </label>
+        <div className="input-block">
           <input
-            className="form__input"
+            // className="form__input"
             type="email"
-            placeholder="Youremail@example.com"
             required
             name="emailAddress"
             value={formData.emailAddress}
             onChange={onChange}
           />
+          <span className="placeholder">Email Address: </span>
         </div>
+
         {/* PASSWORD INPUT */}
-        <div className="form__group">
-          <label className="form__label">Password: </label>
+        <div className="input-block">
           <input
-            className="form__input"
+            // className="form__input"
             type="password"
-            placeholder="• • • • • • • •"
+            // placeholder="• • • • • • • •"
             required
             name="userPassword"
             value={formData.userPassword}
             onChange={onChange}
           />
+          <span className="placeholder">Password: </span>
         </div>
-        <button className="login-btn " onClick={onSubmit}>
+        <button
+          className=" password_btn"
+          style={{ width: '100%', marginTop: '1em' }}
+          onClick={onSubmit}
+        >
           {/* NOTE:'condtional rendering logic for displaying either the loading animation or 'done'  */}
           {authContxt.isLoading ? (
             <div className="spinner_icon">
@@ -74,6 +87,12 @@ const LoginPage = () => {
             'login'
           )}
         </button>
+
+        <div style={{ textAlign: 'center', marginTop: '1em' }}>
+          <Link to="/forgot-password">
+            <p> Forgot password?</p>
+          </Link>
+        </div>
       </form>
       <span className="signup__note">
         Don't have an account? <Link to="/signup">Sign up</Link>

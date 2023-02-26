1053c206240e8a0a4bcda8b273b334a146cf6e2b
dev-sydney <sydneyotutey279@gmail.com>
2023-02-26 14:57:20 +0000

    Page component created for testing purposes

---
+++ view/src/pages/Testing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoadingDeptCourses from '../components/loadingComponents/LoadingDeptCourses';
+const Testing = () => {
+  return (
+    <div style={{ minHeight: 'inherit' }}>
+      <LoadingDeptCourses />
+    </div>
+  );
+};
+
+export default Testing;

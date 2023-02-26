19d973dc2fec1ff00d89b5291fe8f6519202b418
dev-sydney <sydneyotutey279@gmail.com>
2023-02-26 13:22:12 +0000

    Implemented the logic for the conditonal rendering of some of the resource manger controls based on the users privilege

--- view/src/pages/ResourcePage.jsx
+++ view/src/pages/ResourcePage.jsx
@@ -26,96 +26,123 @@ const ResourcePage = () => {
       navigateTo('/', { replace: true });
   }, []);
 
-  const onBtnClick = (state, setStateFn) => () => {
-    setStateFn(!state);
-  };
   return (
     <div className="resource__container">
       <h1 style={{ textAlign: 'left', padding: '.3em .7em' }}>
         Manage
         <br /> Resources
       </h1>
-      <hr />
+
       <div className="controls__container">
-        <div className="control">
-          <div className="icon" style={{ background: '#4d4df02b' }}>
-            <UilGraduationCap
-              size="30"
-              color="#3434E6"
-              style={{ margin: '.5em' }}
-            />
-          </div>
-          <h2>
-            Manage
-            <br /> Professors
-          </h2>
+        {authContxt.user &&
+          (['head_of_department'].includes(authContxt.user.privilege) ? (
+            <div className="control">
+              <div className="icon" style={{ background: '#4d4df02b' }}>
+                <UilGraduationCap
+                  size="30"
+                  color="#3434E6"
+                  style={{ margin: '.5em' }}
+                />
+              </div>
+              <h2>
+                Manage
+                <br /> Professors
+              </h2>
 
-          <Link to="/departmentCourses">
-            <UilArrowRight
-              size="30"
-              color="#FFFFFF"
-              style={{ margin: '.4em' }}
-            />
-          </Link>
-        </div>
+              <Link to="/departmentCourses">
+                <UilArrowRight
+                  size="30"
+                  color="#FFFFFF"
+                  style={{ margin: '.4em' }}
+                />
+              </Link>
+            </div>
+          ) : (
+            ''
+          ))}
 
-        <div className="control">
-          <div className="icon" style={{ background: ' #f9b94a51' }}>
-            <UilPodium size="37" color="#efa92f" style={{ margin: '.5em' }} />
-          </div>
-          <h2>
-            Lecture
-            <br /> Halls
-          </h2>
+        {authContxt.user &&
+          (['admin'].includes(authContxt.user.privilege) ? (
+            <div className="control">
+              <div className="icon" style={{ background: ' #f9b94a51' }}>
+                <UilPodium
+                  size="37"
+                  color="#efa92f"
+                  style={{ margin: '.5em' }}
+                />
+              </div>
+              <h2>
+                Lecture
+                <br /> Halls
+              </h2>
 
-          <Link to="/resourceManager/lecture-halls">
-            <UilArrowRight
-              size="30"
-              color="#FFFFFF"
-              style={{ margin: '.45em' }}
-            />
-          </Link>
-        </div>
+              <Link to="/resourceManager/lecture-halls">
+                <UilArrowRight
+                  size="30"
+                  color="#FFFFFF"
+                  style={{ margin: '.45em' }}
+                />
+              </Link>
+            </div>
+          ) : (
+            ''
+          ))}
 
-        <div className="control">
-          <div className="icon" style={{ background: '#0d903037' }}>
-            <UilBooks size="30" color="#0D9030" style={{ margin: '.7em' }} />
-          </div>
-          <h2>
-            Manage
-            <br /> Courses
-          </h2>
+        {/* NOTE: Conditional rendering of the link to the Manage courses page */}
+        {authContxt.user &&
+          (['head_of_department'].includes(authContxt.user.privilege) ? (
+            <div className="control">
+              <div className="icon" style={{ background: '#0d903037' }}>
+                <UilBooks
+                  size="30"
+                  color="#0D9030"
+                  style={{ margin: '.7em' }}
+                />
+              </div>
+              <h2>
+                Manage
+                <br /> Courses
+              </h2>
 
-          <Link to="/resourceManager/courses">
-            <UilArrowRight
-              size="30"
-              color="#FFFFFF"
-              style={{ margin: '.4em' }}
-            />
-          </Link>
-        </div>
+              <Link to="/resourceManager/courses">
+                <UilArrowRight
+                  size="30"
+                  color="#FFFFFF"
+                  style={{ margin: '.4em' }}
+                />
+              </Link>
+            </div>
+          ) : (
+            ''
+          ))}
 
-        <div className="control">
-          <div className="icon" style={{ background: '#5f10903d' }}>
-            <UilUniversity
-              size="30"
-              color="#5E1090"
-              style={{ margin: '.5em' }}
-            />
-          </div>
-          <h2>
-            Manage
-            <br /> Faculties
-          </h2>
+        {/* NOTE: Conditional rendering of the link to the manage Faculties page */}
+        {authContxt.user &&
+          (['admin'].includes(authContxt.user.privilege) ? (
+            <div className="control">
+              <div className="icon" style={{ background: '#5f10903d' }}>
+                <UilUniversity
+                  size="30"
+                  color="#5E1090"
+                  style={{ margin: '.5em' }}
+                />
+              </div>
+              <h2>
+                Manage
+                <br /> Faculties
+              </h2>
 
-          <Link to="/resourceManager/faculties">
-            <UilArrowRight
-              size="30"
-              color="#FFFFFF"
-              style={{ margin: '.4em' }}
-            />
-          </Link>
-        </div>
+              <Link to="/resourceManager/faculties">
+                <UilArrowRight
+                  size="30"
+                  color="#FFFFFF"
+                  style={{ margin: '.4em' }}
+                />
+              </Link>
+            </div>
+          ) : (
+            ''
+          ))}
       </div>
       <div style={{ minHeight: '5em' }}></div>
     </div>

6fed57e3980d09c33c324228f511fb31c20fead0
dev-sydney <sydneyotutey279@gmail.com>
2023-03-21 22:50:27 +0000

    Linked the scss file to style the component

--- view/src/components/AddLectureRoomForm.jsx
+++ view/src/components/AddLectureRoomForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { UilTimes, UilSpinnerAlt } from '@iconscout/react-unicons';
 
 import resourceContext from '../contexts/ResourceContext';
-
+import './../styles/resourceFormStyle.scss';
 const AddLectureRoomForm = ({ isModalActive, setIsModalActive }) => {
   const resourceContxt = useContext(resourceContext);
 
